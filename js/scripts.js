023fd8d868ccad22a7ca5bdbb66eb16289b26d47
Jeremy Canela <jeremycanela@gmail.com>
2018-12-01 21:32:04 -0500

    Unobtrusive javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
-$(".project").click((event) => {
-	console.log($(this));
-	window.location.href = $(event.target).parent(".project").attr("data-href");
-});
+// Logo animation
+$(".logo").css("height", $(".logo div").height());
 
+$(".logo div").css({
+	"position": "absolute",
+	"top" : "40px",
+	"left": "0",
+    "right": "0"
+}).animate({
+    top: 0
+}, 1000);
+
+// Tilt.js
 $(".project .view").tilt({
     scale: 1.08,
     glare: true,
     maxGlare: .5
 });
 
+// Projects
 $(".project .view").on("mouseover", (event) => {
 	$(".project .view").parent(".project").css("z-index", "0");
 	$(event.target).parent(".project").css("z-index", "1");
 });
-
-$(".logo").css("height", $(".logo div").height());
-
-$(".logo div").animate({
-	top: 0
-}, 1000);
